f9722ae6ca18d34274b0a81997937e563f2cb546
Manuel Bovo <manuel.bovo@gmail.com>
2021-05-17 12:08:27 +0200

    Fix publish workdir

--- publish.sh
+++ publish.sh
@@ -14,5 +14,5 @@ if jfrog rt s "$HELM_PUSH_REPO/$CHART_NAME-$VERSION.tgz" --url $REGISTRY --apike
     exit 0
 else
     printf "==> Attempting to upload:\n$CHART_NAME-$VERSION.tgz\nto $REGISTRY/${HELM_PUSH_REPO}\n"
-    jfrog rt u $CHART_DIR/$CHART_NAME-$VERSION.tgz $HELM_PUSH_REPO --url $REGISTRY --apikey $ARTIFACTORY_PASSWORD --fail-no-op || exit 1
+    jfrog rt u $RUNNER_WORKSPACE/$CHART_DIR/$CHART_NAME-$VERSION.tgz $HELM_PUSH_REPO --url $REGISTRY --apikey $ARTIFACTORY_PASSWORD --fail-no-op || exit 1
 fi
