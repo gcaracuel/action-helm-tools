c4510cc8c0454048d1f2b584bb7ac9940991da8e
Manuel Bovo <manuel.bovo@gmail.com>
2021-05-17 12:20:43 +0200

    Fix publish workdir

--- publish.sh
+++ publish.sh
@@ -9,10 +9,10 @@ install_jfrog
 
 echo "==> Check published version of $CHART_NAME"
 
-if jfrog rt s "$HELM_PUSH_REPO/$CHART_NAME-$VERSION.tgz" --url $REGISTRY --apikey $ARTIFACTORY_PASSWORD --fail-no-op; then
-    printf "$CHART_NAME-$VERSION already exist in artifactory $HELM_PUSH_REPO, exit\n"
+if jfrog rt s "$HELM_PUSH_REPO/$CHART_NAME-v$VERSION.tgz" --url $REGISTRY --apikey $ARTIFACTORY_PASSWORD --fail-no-op; then
+    printf "$CHART_NAME-v$VERSION already exist in artifactory $HELM_PUSH_REPO, exit\n"
     exit 0
 else
     printf "==> Attempting to upload:\n$CHART_NAME-$VERSION.tgz\nto $REGISTRY/${HELM_PUSH_REPO}\n"
-    jfrog rt u $RUNNER_WORKSPACE/$CHART_NAME-$VERSION.tgz $HELM_PUSH_REPO --url $REGISTRY --apikey $ARTIFACTORY_PASSWORD --fail-no-op || exit 1
+    jfrog rt u $RUNNER_WORKSPACE/$CHART_NAME-v$VERSION.tgz $HELM_PUSH_REPO --url $REGISTRY --apikey $ARTIFACTORY_PASSWORD --fail-no-op || exit 1
 fi
